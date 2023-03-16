167ce352182cff71f125505da702b967946d662f
Hasan <programmermdhasan@gmail.com>
2023-03-16 14:23:34 +0600

    completed lesson6

---
+++ Lesson6/index.js
@@ -0,0 +1,6 @@
+const fruitsName = require('random-fruits-name');
+const randomMovieNames = require('random-movie-names');
+
+console.log(fruitsName('en'));
+console.log(randomMovieNames(), " = random movie")
+
