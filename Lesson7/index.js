06585de71583dead0d6e02d71eb4294edd916412
Hasan <programmermdhasan@gmail.com>
2023-03-20 08:51:46 +0600

    working lesson7

--- Lesson7/index.js
+++ Lesson7/index.js
@@ -1,13 +1,40 @@
 const http = require("http");
+const file = require("fs");
 
-const port = 3200;
-const hostname = '127.0.0.1';
+const PORT = 3200;
+const hostName = "127.0.0.1";
 
 const server = http.createServer((req, res) => {
-    res.end("<h1>Welcome to the server!!</h1>")
-})
+  console.log(req.url, "url");
+  const url = req.url;
 
+  if (url === "/") {
+    file.readFile("./views/home.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (url === "/about") {
+    file.readFile("./views/about.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (url === "/contact") {
+    file.readFile("./views/contact.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    file.readFile("./views/error.html", (err, data) => {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
 
-server.listen(port, hostname, () => {
-    console.log(`server is running at http://${hostname}:${port}`)
-})
+server.listen(PORT, hostName, () => {
+  console.log(`server is running at http://${hostName}:${PORT}`);
+});
